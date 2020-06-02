d6e94791b391d6745cc50feb587b1ccbcb65c0f1
forijk <forijk@163.com>
2020-06-03 00:58:49 +0800

    InputTags

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,75 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <a-form :form="form" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+    <a-form-item label="Note">
+      <!-- <a-input
+        v-decorator="['note', { rules: [{ required: true, message: 'Please input your note!' }] }]"
+      /> -->
+
+      <input-tags v-decorator="['note', { rules: [{ required: true, message: 'Please input your note!' }] }]" @input="handleInput"></input-tags>
+      
+    </a-form-item>
+    <a-form-item label="Gender">
+      <a-select
+        v-decorator="[
+          'gender',
+          { rules: [{ required: true, message: 'Please select your gender!' }] },
+        ]"
+        placeholder="Select a option and change input text above"
+        @change="handleSelectChange"
+      >
+        <a-select-option value="male">
+          male
+        </a-select-option>
+        <a-select-option value="female">
+          female
+        </a-select-option>
+      </a-select>
+    </a-form-item>
+    <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+      <a-button type="primary" html-type="submit" @click.native="handleSubmit">
+        Submit
+      </a-button>
+    </a-form-item>
+  </a-form>
 </template>
+
+<script>
+import InputTags from './components/InputTags'
+
+export default {
+  data() {
+    return {
+      formLayout: 'horizontal',
+      form: this.$form.createForm(this, { name: 'coordinated' }),
+      arr: []
+    };
+  },
+  components: {
+    InputTags
+  },
+  methods: {
+    handleInput(v, isAdd) {
+      this.arr = isAdd ? this.arr.concat(v) : this.arr.filter(item => item !==v)
+      this.form.setFieldsValue({
+        note: this.arr
+      });
+      console.log(this.arr)
+    },
+    handleSubmit(e) {
+      console.log(1111)
+      e.preventDefault();
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+    },
+    handleSelectChange(value) {
+      console.log(value);
+      this.form.setFieldsValue({
+        note: ['haha']
+      });
+    },
+  },
+};
+</script>
---
+++ src/views/components/InputTags/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <a-tag color="#108ee9" v-for="(tag, index) in value" :key="tag + index" closable @close="del(tag)">
+            {{ tag }}
+        </a-tag>
+        <a-input placeholder="input placeholder" @keyup.enter="add" v-model="inputValue"/>
+    </div>
+</template>
+<script>
+export default {
+    props: ['value'],
+    data() {
+        return {
+            inputValue: ''
+        }
+    },
+    methods: {
+        add() {
+            console.log(this.inputValue)
+            this.$emit('input', this.inputValue, true)
+            this.inputValue = ''
+        },
+        del(tag) {
+            this.$emit('input', tag, false)
+        }
+    }
+}
+</script>
+<style scoped>
+    
+</style>
